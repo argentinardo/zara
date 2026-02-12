4f6a0c7388f3cc1af2de6e2a2dc5b6250c3e4721
argentinardo <damiannardini@gmail.com>
2026-02-12 23:23:01 +0100

    feat(types): add types for phone and api messages

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type {
+  Phone,
+  PhoneListItem,
+  Specs,
+  ColorOption,
+  StorageOption,
+  SimilarProduct,
+} from './phone'
+
+export type {
+  ApiErrorResponse,
+} from './api'
---
+++ src/types/phone.ts
@@ -0,0 +1,44 @@
+export interface Specs {
+  screen: string
+  resolution: string
+  processor: string
+  mainCamera: string
+  selfieCamera: string
+  battery: string
+  os: string
+  screenRefreshRate: string
+}
+
+export interface ColorOption {
+  name: string
+  hexCode: string
+  imageUrl: string
+}
+
+export interface StorageOption {
+  capacity: string
+  price: number
+}
+
+export interface PhoneListItem {
+  id: string
+  brand: string
+  name: string
+  basePrice: number
+  imageUrl: string
+}
+
+export type SimilarProduct = PhoneListItem
+
+export interface Phone {
+  id: string
+  brand: string
+  name: string
+  description: string
+  basePrice: number
+  rating: number
+  specs: Specs
+  colorOptions: ColorOption[]
+  storageOptions: StorageOption[]
+  similarProducts: SimilarProduct[]
+}
