171edee30d244ea8efee3217649026aaa39b1d83
argentinardo <damiannardini@gmail.com>
2026-02-14 17:49:24 +0100

    feat: add React context API for cart state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { CartProvider } from '@/context/CartContext'
+import Cart from './components/Cart'
 
 function App() {
   return (
     <CartProvider>
       <h1>Zara</h1>
+      <Cart />
     </CartProvider>
   )
 }
