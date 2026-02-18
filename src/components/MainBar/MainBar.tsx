a86b0fe105ed71013218a7a610a55a60e7c75cfc
argentinardo <damiannardini@gmail.com>
2026-02-18 02:08:30 +0100

    feat(MainBar): add back button

--- src/components/MainBar/MainBar.tsx
+++ src/components/MainBar/MainBar.tsx
@@ -1,17 +1,30 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useMatch, useNavigate } from "react-router-dom"
 import logo from "../../assets/logo.svg"
 import CartBadge from "../CartBadge/CartBadge"
 
 const MainBar = () => {
+  const isProductSpecs = useMatch('/products/:id')
+  const navigate = useNavigate()
+
   return (
     <div className="main-bar">
-      <NavLink to="/" >
+      <NavLink to="/">
         <img src={logo} alt="" />
       </NavLink>
-      <NavLink to="/cart" >
+      <NavLink to="/cart">
         <CartBadge />
       </NavLink>
+      {isProductSpecs && (
+        <button
+          type="button"
+          className="main-bar__back"
+          onClick={() => navigate(-1)}
+          aria-label="Volver"
+        >
+          back
+        </button>
+      )}
     </div>
-    )
+  )
 }
 export default MainBar
