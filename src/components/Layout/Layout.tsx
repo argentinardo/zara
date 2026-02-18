c8362547a2130a2747ca954d1c822a2256f2b3db
argentinardo <damiannardini@gmail.com>
2026-02-18 14:45:04 +0100

    feat(MainBar): add MainBar styles for PhoneList component & move cartBadge component into mainbar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import MainBar from "../MainBar/MainBar";
+import MainBar from "@/components/MainBar"
 
 function Layout() {
   return (
--- src/components/MainBar/MainBar.tsx
+++ src/components/MainBar/MainBar.tsx
@@ -1,30 +1,38 @@
 import { NavLink, useMatch, useNavigate } from "react-router-dom"
 import logo from "../../assets/logo.svg"
-import CartBadge from "../CartBadge/CartBadge"
+import { useContext } from 'react'
+import { CartContext } from '@/context/CartContext'
+import cartFill from "@/assets/cart-fill.svg"
+import cartEmpty from "@/assets/cart-empty.svg"
+import chevronLeft from "@/assets/chevron-left.svg"
 
+
+    
 const MainBar = () => {
+  const { cart } = useContext(CartContext)
   const isProductSpecs = useMatch('/products/:id')
   const navigate = useNavigate()
 
   return (
     <div className="main-bar">
       <NavLink to="/">
-        <img src={logo} alt="" />
+        <img className="main-bar__logo" src={logo} alt="" />
       </NavLink>
-      <NavLink to="/cart">
-        <CartBadge />
+      <NavLink className="main-bar__cart-badge" to="/cart">
+        <img className="main-bar__cart-icon" src={cart.length > 0 ? cartFill : cartEmpty} />
+        <div className="main-bar__cart-text">{cart.length}</div>
       </NavLink>
       {isProductSpecs && (
-        <button
-          type="button"
-          className="main-bar__back"
-          onClick={() => navigate(-1)}
-          aria-label="Volver"
-        >
-          back
-        </button>
+        <div className="main-bar__footer">
+          <button
+            className="main-bar__back"
+            onClick={() => navigate(-1)}
+            aria-label="Volver"
+          ><img src={chevronLeft} alt="" />back
+          </button>
+        </div>
       )}
     </div>
   )
 }
-export default MainBar
+export default MainBar    
